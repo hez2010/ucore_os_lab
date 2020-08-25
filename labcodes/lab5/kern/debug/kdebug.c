9c6f88c1fd4067736e32f4bb0aea82c5bb40bedb
Sawada Katsuya <hez2010@outlook.com>
2020-08-25 22:16:32 +0800

    lab5

--- labcodes/lab5/kern/debug/kdebug.c
+++ labcodes/lab5/kern/debug/kdebug.c
@@ -335,17 +335,18 @@ read_eip(void) {
  * */
 void
 print_stackframe(void) {
-     /* LAB1 YOUR CODE : STEP 1 */
-     /* (1) call read_ebp() to get the value of ebp. the type is (uint32_t);
-      * (2) call read_eip() to get the value of eip. the type is (uint32_t);
-      * (3) from 0 .. STACKFRAME_DEPTH
-      *    (3.1) printf value of ebp, eip
-      *    (3.2) (uint32_t)calling arguments [0..4] = the contents in address (uint32_t)ebp +2 [0..4]
-      *    (3.3) cprintf("\n");
-      *    (3.4) call print_debuginfo(eip-1) to print the C calling function name and line number, etc.
-      *    (3.5) popup a calling stackframe
-      *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
-      *                   the calling funciton's ebp = ss:[ebp]
-      */
+    uint32_t ebp = read_ebp();
+    uint32_t eip = read_eip();
+    for (int i = 0; i < STACKFRAME_DEPTH; i++) {
+        if (ebp == 0) break;
+        cprintf("ebp: 0x%08x, eip: 0x%08x, args: ", ebp, eip);
+        uint32_t* baseAddr = (uint32_t*)ebp;
+        for (int j = 0; j < 4; j++) {
+            cprintf("0x%08x%s", *(baseAddr + 2 + j), j == 3 ? "\n" : ", ");
+        }
+        print_debuginfo(eip - 1);
+        eip = *(baseAddr + 1);
+        ebp = *baseAddr;
+    }
 }
 
